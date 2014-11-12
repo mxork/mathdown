f5b9e532e8d5f9fc0ecb7afe1923842736f92419
Dan Anderson <dan.hillier.anderson@gmail.com>
2014-11-12 11:53:40 -0500

    can't remember

--- main.go
+++ main.go
@@ -3,10 +3,13 @@ package main
 import (
 	"fmt"
 	"mathdown/lex"
+	"mathdown/parse"
 )
 
 func main() {
 	l := lex.New([]byte("happy.once+joos`2 = great / 2 ; tt"))
 	ts := l.Lex()
 	fmt.Println(ts)
+	p := parse.New(ts)
+	fmt.Println(parse.ToRx(p.Main))
 }
--- parse/parse.go
+++ parse/parse.go
@@ -2,4 +2,65 @@ package parse
 
 import (
 	"mathdown/lex"
+	re "regexp"
 )
+
+var rxs = map[*re.Regexp]func([][])
+
+type Phrase interface {
+	String() string
+	Rx() string
+}
+
+type Expr interface {
+	Phrase
+}
+
+type Term struct {
+	lex.Token
+}
+
+func (t Term) Rx() string {
+	return t.Ilk.String()
+}
+
+type Binop struct {
+	Term
+	Left, Right Expr
+}
+
+func (b Binop) Rx() string {
+	return "EXPR"
+}
+
+type Parser struct {
+	Main, Alt []Phrase
+	indices map[int]int // maps string index to real
+}
+
+func (p Parser) toreal(x int) {
+	return p.indices[i]
+}
+
+func New(toks []lex.Token) Parser {
+	main := make([]Phrase, 0, len(toks))
+	alt := make([]Phrase, 0, len(toks))
+
+	for _, v := range toks {
+		main = append(main, Term{v})
+	}
+
+	return Parser{main, alt}
+}
+
+// Ready for shameless abuse of the regexp package? I am.
+func ToRx(ps []Phrase) string {
+	s := ""
+	for i, v := range ps {
+		s += v.Rx()
+		if i != len(ps)-1 {
+			s += " "
+		}
+	}
+	return s
+}
